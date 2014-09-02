2813f7aa59dac7f918ba291b4f56633c76c4ee61
pittMarson <pierre.mardon@gmail.com>
2014-09-02 14:35:09 +0200

    Switched from ngmin (deprecated) to ng-annotate in ui (sample and archetype), closes #164

--- net.osgiliath.samples/net.osgiliath.hello/net.osgiliath.hello.ui/Gruntfile.js
+++ net.osgiliath.samples/net.osgiliath.hello/net.osgiliath.hello.ui/Gruntfile.js
@@ -252,7 +252,7 @@ module.exports = function (grunt) {
 
     // Allow the use of non-minsafe AngularJS files. Automatically makes it
     // minsafe compatible so Uglify does not destroy the ng references
-    ngmin: {
+    ngAnnotate: {
       dist: {
         files: [{
           expand: true,
@@ -395,7 +395,7 @@ module.exports = function (grunt) {
     'concurrent:dist',
     'autoprefixer',
     'concat',
-    'ngmin',
+    'ngAnnotate',
     'copy:dist',
     'cdnify',
     'cssmin',
